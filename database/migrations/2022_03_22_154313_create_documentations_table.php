ec8007bd427f4a40b8a7f6c1c9eee8b2c48fde86
mani143gg <manikandan143g@gmail.com>
2022-03-25 00:35:22 +0530

    first commit

---
+++ database/migrations/2022_03_22_154313_create_documentations_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDocumentationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('documentations', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 100);
+            $table->longText('to_address');
+            $table->timestamps();
+            $table->dateTime('created_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('documentations');
+    }
+}
